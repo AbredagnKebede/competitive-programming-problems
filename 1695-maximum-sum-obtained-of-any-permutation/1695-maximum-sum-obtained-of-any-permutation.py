b57e825221f15805611775297ad41ed90b9481be
Abredagn <162739236+AbredagnKebede@users.noreply.github.com>
2025-02-20 11:07:11 +0300

    Time: 68 ms (99.49%), Space: 45.2 MB (24.87%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.py
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.py
@@ -2,28 +2,11 @@ class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
         nums.sort(reverse=True)
         prefix = [0]*(len(nums)+1)
+
         for start, end in requests:
-            prefix[start] += -1
-            prefix[end + 1] += 1
+            prefix[start] -= 1
+            prefix[end+1] += 1
 
-        hashmap = defaultdict(int)
-        add = 0
-        for i in reversed(range(1, len(prefix))):
-            add += prefix[i]
-            hashmap[i-1] += add 
-
-        hashmap = dict(sorted(hashmap.items(), key = lambda item: item[1], reverse=True))
-        ans = [0]*len(nums)
-        cur = 0
-        for i in hashmap:
-            ans[i] = nums[cur]
-            cur += 1
-        for _ in range(1, len(ans)):
-            ans[_] += ans[_-1]
-
-        max_sum = 0
-        for start, end in requests:
-            max_sum += (ans[end] - ans[start - 1]) if start != 0 else ans[end]
-
-        return max_sum % (10**9 + 7)
+        prefix = sorted(list(itertools.accumulate(reversed(prefix))), reverse=True)
         
+        return sum(count * value for count, value in zip(prefix, nums)) % (10**9 + 7)
