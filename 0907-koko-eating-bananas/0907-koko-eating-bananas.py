8c16dc0c1dcd1c22c81d58b55edc093042234dc4
Abredagn <162739236+AbredagnKebede@users.noreply.github.com>
2025-05-19 21:28:56 +0300

    Time: 145 ms (86.81%), Space: 18.3 MB (99.98%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -2,12 +2,8 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         def canEatAll(speed):
             total_hours = 0
-            for i in piles:
-                if i <= speed:
-                    total_hours +=1
-                elif i > speed:
-                    if i%speed !=0: total_hours += (i//speed + 1)
-                    else: total_hours +=(i//speed) 
+            for pile in piles:
+                total_hours += (pile + speed - 1) // speed
             return total_hours <= h
 
         low, high = 1, max(piles)
