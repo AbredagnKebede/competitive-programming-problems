9027f5831e0a482fc3448869b05d9a3d482a0231
Abredagn <162739236+AbredagnKebede@users.noreply.github.com>
2025-05-28 21:46:36 +0300

    Time: 569 ms (5.03%), Space: 27.8 MB (94.96%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List
+    [float], start_node: int, end_node: int) -> float:
+        distance = [0]*n
+        distance[start_node] = 1
+        for i in range(n-1):
+            flag = False
+            for j, (k, l) in enumerate(edges):
+                if distance[k] * succProb[j] > distance[l]:
+                    distance[l] = distance[k] * succProb[j]
+                    flag = True
+                if distance[l] * succProb[j] > distance[k]:
+                    distance[k] = distance[l] * succProb[j]
+                    falg = True
+            if not flag:
+                break
+        
+        return distance[end_node]
