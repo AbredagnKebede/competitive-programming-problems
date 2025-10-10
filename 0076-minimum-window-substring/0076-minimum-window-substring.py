450577784142f6681dd590f0ca40ff93dbe673f5
Abredagn <162739236+AbredagnKebede@users.noreply.github.com>
2025-10-10 23:19:54 +0300

    Time: 78 ms (50.5%), Space: 17.2 MB (100%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_cnt = Counter(t)
-        s_cnt = defaultdict(int)
-        have , need = 0, len(t_cnt)
-        min_len = float('inf')
-        min_window = ""
-        left = 0
-        for right in range(len(s)):
-            cur = s[right]
-            s_cnt[cur] += 1
-
-            if cur in t_cnt and s_cnt[cur] == t_cnt[cur]:
-                have += 1
-            
-            while have == need:
-                if (right - left + 1) < min_len:
-                    min_len = right - left + 1
-                    min_window = s[left:right + 1]
-
-                s_cnt[s[left]] -= 1
-                if s[left] in t_cnt and s_cnt[s[left]] < t_cnt[s[left]]:
-                    have -= 1
-                left += 1 
-        
-        return min_window
+        if len(s) < len(t):
+            return ""
+        needstr = collections.defaultdict(int)
+        for ch in t:
+            needstr[ch] += 1
+        needcnt = len(t)
+        res = (0, float('inf'))
+        start = 0
+        for end, ch in enumerate(s):
+            if needstr[ch] > 0:
+                needcnt -= 1
+            needstr[ch] -= 1
+            if needcnt == 0:
+                while True:
+                    tmp = s[start]
+                    if needstr[tmp] == 0:
+                        break
+                    needstr[tmp] += 1
+                    start += 1
+                if end - start < res[1] - res[0]:
+                    res = (start, end)
+                needstr[s[start]] += 1
+                needcnt += 1
+                start += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
